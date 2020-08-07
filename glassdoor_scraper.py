556585f83b887bf908a06e47b89e387bc276546d
Ruth ohobi <ruthohobi@outlook.com>
2020-08-07 01:12:37 -0400

    data cleaning script created

--- glassdoor_scraper.py
+++ glassdoor_scraper.py
@@ -26,7 +26,7 @@ def get_jobs(keyword, num_jobs, verbose, path, slp_time):
     driver = webdriver.Chrome(executable_path=path, options=options)
     driver.set_window_size(1120, 1000)
 
-    url = "https://www.glassdoor.ca/Job/data-scientist-jobs-SRCH_KE0,14.htm"
+    url = "https://www.glassdoor.ca/Job/us-data-scientist-jobs-SRCH_IL.0,2_IN1_KE3,17.htm"
     #url = 'https://www.glassdoor.com/Job/jobs.htm?sc.keyword="' + keyword + '"&locT=C&locId=1147401&locKeyword=San%20Francisco,%20CA&jobType=all&fromAge=-1&minSalary=0&includeNoSalaryJobs=true&radius=100&cityId=-1&minRating=0.0&industryId=-1&sgocId=-1&seniorityType=all&companyId=-1&employerSizes=0&applicationType=0&remoteWorkType=0'
     driver.get(url)
     jobs = []
@@ -46,7 +46,8 @@ def get_jobs(keyword, num_jobs, verbose, path, slp_time):
         time.sleep(.1)
 
         try:
-            driver.find_element_by_css_selector('[alt="Close"]').click()  #clicking to the X.
+            driver.find_element_by_xpath('.//span[@class="SVGInline modal_closeIcon"]').click()
+            #driver.find_element_by_css_selector('[alt="Close"]').click()  #clicking to the X.
             print(' x out worked')
         except NoSuchElementException:
             print(' x out failed')
@@ -76,7 +77,10 @@ def get_jobs(keyword, num_jobs, verbose, path, slp_time):
                     time.sleep(5)
 
             try:
-                salary_estimate = driver.find_element_by_xpath('.//span[@class="gray salary"]').text
+                #salary_estimate = driver.find_element_by_xpath('.//span[@class="gray salary"]').text
+                salary_estimate = driver.find_element_by_xpath('.//div[@class="salary"]').text
+                #salary_estimate = driver.find_element_by_xpath('.//span[@class="salary"]').text
+                #print('salary_estimate')
             except NoSuchElementException:
                 salary_estimate = -1 #You need to set a "not found value. It's important."
             
